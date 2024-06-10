592a90c8a0acca117dcf255ed4af92dded3936b0
daniel-bota <118935827+daniel-bota@users.noreply.github.com>
2024-06-10 08:50:24 +0300

    implemented authentication

---
+++ NotesApp/Core/Authentication/IAuthenticationManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApp.Data.Authentication;
+using NotesApp.Data;
+using NotesApp.Models.Authentication;
+
+namespace NotesApp.Core.Authentication
+{
+    public interface IAuthenticationManager
+    {
+        public string AuthenticatedUsername { get; }
+        public string SessionId { get; }
+
+
+        /// <summary>
+        ///     Adds a new user to the database.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>
+        ///     string.Empty if successful.Error message if failed.
+        /// </returns>
+        Task<string> RegisterNewUser(RegistrationViewModel model);
+
+        Task<bool> ValidateAuthenticationClaim(LoginViewModel model);
+
+        /// <summary>
+        ///     Adds a new authentication session to the database.
+        ///     The session will have a user id that matches the user 
+        ///     with the currently set AuthenticatedUsername.
+        ///     Stores the session id hash in the SessionId property.
+        ///     This will be used to create the login cookie by the controller.
+        /// </summary>
+        /// <returns>
+        ///     string.Empty if successful.Error message if failed.
+        /// </returns>
+        Task<string> CreateAuthenticationSession();
+        Task<bool> ValidateAuthenticationCookie(string sessionKey);
+        Task<bool> ValidateSessionUsername(string username);
+
+
+    }
+}
---
+++ NotesApp/Data/Authentication/UserAccount.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using NotesApp.Data.Note;
+
+namespace NotesApp.Data.Authentication
+{
+    [Index(nameof(Username), IsUnique = true)]
+    [Index(nameof(Email), IsUnique = true)]
+    public class UserAccount
+	{
+		public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Username must not be empty.")]
+        [MaxLength(100, ErrorMessage = "Username most not be longer than 100 characters.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Email must not be empty.")]
+        [MaxLength(200, ErrorMessage = "Email address must not be longer than 200 characters.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password must not be empty.")]
+        [Length(8, 40, ErrorMessage = "Password must be between 8 and 40 characters long.")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "User-specific encryption salt was not provided.")]
+        public string? PasswordSalt { get; set; }
+
+        public virtual ICollection<UserAuthenticationSession>? UserAuthenticationSessions { get; }
+        public virtual ICollection<UserNote>? Notes { get; }
+    }
+}
