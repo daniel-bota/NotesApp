b32b6a96d263aa75649b4d6c85f28e71209ac024
daniel-bota <118935827+daniel-bota@users.noreply.github.com>
2024-06-10 09:53:46 +0300

    added notes

---
+++ NotesApp/Data/Note/UserNote.cs
@@ -0,0 +1,21 @@
+﻿using NotesApp.Data.Authentication;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NotesApp.Data.Note
+{
+    public class UserNote
+    {
+        public Guid Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Content { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        [ForeignKey("UserAccount")]
+        public Guid UserAccountId { get; set; }
+
+        public virtual UserAccount? UserAccount { get; set; }
+    }
+}
---
+++ NotesApp/Migrations/20240610065206_InitialMigration.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    PasswordSalt = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAuthenticationSessions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserAccountId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAuthenticationSessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAuthenticationSessions_UserAccounts_UserAccountId",
+                        column: x => x.UserAccountId,
+                        principalTable: "UserAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserNotes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserAccountId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserNotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserNotes_UserAccounts_UserAccountId",
+                        column: x => x.UserAccountId,
+                        principalTable: "UserAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAccounts_Email",
+                table: "UserAccounts",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAccounts_Username",
+                table: "UserAccounts",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAuthenticationSessions_UserAccountId",
+                table: "UserAuthenticationSessions",
+                column: "UserAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserNotes_UserAccountId",
+                table: "UserNotes",
+                column: "UserAccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAuthenticationSessions");
+
+            migrationBuilder.DropTable(
+                name: "UserNotes");
+
+            migrationBuilder.DropTable(
+                name: "UserAccounts");
+        }
+    }
+}
